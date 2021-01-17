2b191903ec8c040b6eca92a788cae1c8209e3aeb
marcinw98 <marcin.wisniewski1998@gmail>
2021-01-17 18:27:55 +0100

    poprawa register

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,8 @@ const User = require('../models/user');
 router.post('/register', async (req,res) => {
     const user = new User({
         name: req.body.name,
-        email: request.body.email,
-        password: request.body.password
+        email: req.body.email,
+        password: req.body.password
     });
     try{
         const savedUser = await user.save();
