ddc6988bbffe8a5b02c654751887e95394f2bc77
Paweł Szczodrowski <pawelszczo@gmail.com>
2021-01-18 16:07:11 +0100

    some validation issues

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const Joi = require('@hapi/joi');
+
+const schema = {
+    name: Joi.string().min(5).required(),
+    email: Joi.string().email().required(),
+    password:Joi.string().min(6).required
+}
 
 router.post('/register', async (req,res) => {
+    //const {error} = Joi.validate(req.body, schema);
+   // res.send(error.details[0].message);
     const user = new User({
         name: req.body.name,
         email: req.body.email,
