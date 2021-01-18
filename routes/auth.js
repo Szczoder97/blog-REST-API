0c59b8d620a69ead0dc962f18e11fe51f97dbaf8
marcinw98 <marcin.wisniewski1998@gmail>
2021-01-18 16:47:52 +0100

    validation issue solved

--- routes/auth.js
+++ routes/auth.js
@@ -3,15 +3,21 @@ const router = express.Router();
 const User = require('../models/user');
 const Joi = require('@hapi/joi');
 
-const schema = {
+const schema = Joi.object({
     name: Joi.string().min(5).required(),
     email: Joi.string().email().required(),
-    password:Joi.string().min(6).required
-}
+    password: Joi.string().min(6).required()
+});
 
 router.post('/register', async (req,res) => {
-    //const {error} = Joi.validate(req.body, schema);
-   // res.send(error.details[0].message);
+    try {
+        const value = await schema.validateAsync(req.body);
+    }
+    catch (err) {res.send(err); }
+
+
+   // const {error} = schema.validate(req.body);
+    
     const user = new User({
         name: req.body.name,
         email: req.body.email,
